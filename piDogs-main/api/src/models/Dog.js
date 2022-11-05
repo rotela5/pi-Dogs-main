4d4dced515dae5c047856071d6a4e4790223e196
Rotela Domingo Ramon <74286462+rotela5@users.noreply.github.com>
2022-11-04 23:18:36 -0300

    final

---
+++ piDogs-main/api/src/models/Dog.js
@@ -0,0 +1,45 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('dog', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
+    height:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    weight: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    life_span :{
+      type: DataTypes.STRING,
+      defaultValue:'No information provided'
+
+    },
+    image: {
+    type: DataTypes.STRING,
+    defaultValue:'https://cdn2.thedogapi.com/images/B12BnxcVQ.jpg'
+    },
+    builtInLocal: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    }
+  },
+  {
+    timestamps: false
+  });
+
+};
