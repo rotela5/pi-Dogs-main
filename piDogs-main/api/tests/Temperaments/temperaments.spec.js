0c97a5ec36d54e5dd806e365418e17da4637d58f
Rotela Domingo Ramon <74286462+rotela5@users.noreply.github.com>
2022-11-07 04:47:11 -0300

    test

--- piDogs-main/api/tests/Temperaments/temperaments.spec.js
+++ piDogs-main/api/tests/Temperaments/temperaments.spec.js
@@ -1,8 +1,8 @@
 import { expect } from 'chai';
 import sequelize from '../../../config/db.config.js';
-import dog from '../../../models/dog.js';
+import temperament from '../../../models/temperament.js';
 
-describe('dog model', () => {
+describe('temperament model', () => {
 	before(() =>
 		sequelize.authenticate().catch((err) => {
 			throw new Error('Unable to connect to the database:', err);
@@ -13,15 +13,13 @@ describe('dog model', () => {
 		let model;
 
 		beforeEach(async () => {
-			model = await dog.create({
-				name: 'Affenpinscher',
-				lowest_weight: 3,
-				
-					});
+			model = await temperament.create({
+				name: 'Watchful',
+			});
 		});
 
 		describe('dataTypes id', () => {
-			beforeEach(() => dog.sync({ force: true }));
+			beforeEach(() => temperament.sync({ force: true }));
 
 			it('should not have a null ID value', () => {
 				expect(model)
@@ -30,16 +28,16 @@ describe('dog model', () => {
 					.to.not.be.a('null');
 			});
 
-			it('should the ID be a valid UUID format', () => {
+			it('should the ID be of type number', () => {
 				expect(model)
 					.to.have.ownPropertyDescriptor('dataValues')
 					.to.have.nested.property('value.id')
-					.to.have.lengthOf(36);
+					.to.be.a('number');
 			});
 		});
 
 		describe('dataTypes name', () => {
-			beforeEach(() => dog.sync({ force: true }));
+			beforeEach(() => temperament.sync({ force: true }));
 
 			it('should not have a null NAME value', () => {
 				expect(model)
@@ -48,25 +46,12 @@ describe('dog model', () => {
 					.to.not.be.a('null');
 			});
 
-			it('should the NAME be of type "string"', () => {
+			it('should the ID be of type "string"', () => {
 				expect(model)
 					.to.have.ownPropertyDescriptor('dataValues')
 					.to.have.nested.property('value.name')
-					.that.is.a('string');
-			});
-
-			it('should the NAME have length max 128 and min 1', () => {
-				expect(model)
-					.to.have.ownPropertyDescriptor('dataValues')
-					.to.have.nested.property('value.name')
-					.to.have.lengthOf.above(1);
-				expect(model)
-					.to.have.ownPropertyDescriptor('dataValues')
-					.to.have.nested.property('value.name')
-					.to.have.lengthOf.below(128);
+					.to.be.a('string');
 			});
 		});
-
-	
 	});
 });
